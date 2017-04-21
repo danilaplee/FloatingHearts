a4b67a82455e8d536d0cf42efc9cb83889bb102a
Daniel <fiend.of.sound@gmail.com>
2017-04-21 19:14:33 +0300

    upgraded to swift 3.0

--- FloatingHeartsExample/ViewController.swift
+++ FloatingHeartsExample/ViewController.swift
@@ -11,14 +11,15 @@ import FloatingHearts
 
 class ViewController: UIViewController {
 
-    private struct HeartAttributes {
-        static let heartSize: CGFloat = 36
-        static let burstDelay: NSTimeInterval = 0.1
+    fileprivate struct HeartAttributes {
+        static let heartSize: CGFloat = 24
+        static let burstDelay: TimeInterval = 0.1
     }
 
-    var burstTimer: NSTimer?
+    var burstTimer: Timer?
 
     override func viewDidLoad() {
+        print("did load view")
         super.viewDidLoad()
 
         view.backgroundColor = UIColor(hex: 0xf2f4f6)
@@ -31,23 +32,23 @@ class ViewController: UIViewController {
         view.addGestureRecognizer(longPressGesture)
     }
 
-    func didLongPress(longPressGesture: UILongPressGestureRecognizer) {
+    func didLongPress(_ longPressGesture: UILongPressGestureRecognizer) {
         switch longPressGesture.state {
-        case .Began:
-            burstTimer = NSTimer.scheduledTimerWithTimeInterval(HeartAttributes.burstDelay, target: self, selector: #selector(showTheLove), userInfo: nil, repeats: true)
-        case .Ended, .Cancelled:
+        case .began:
+            burstTimer = Timer.scheduledTimer(timeInterval: HeartAttributes.burstDelay, target: self, selector: #selector(showTheLove), userInfo: nil, repeats: true)
+        case .ended, .cancelled:
             burstTimer?.invalidate()
         default:
             break
         }
     }
 
-    func showTheLove(gesture: UITapGestureRecognizer?) {
-        let heart = HeartView(frame: CGRectMake(0, 0, HeartAttributes.heartSize, HeartAttributes.heartSize))
+    func showTheLove(_ gesture: UITapGestureRecognizer?) {
+        let heart = HeartView(frame: CGRect(x: 0, y: 0, width: HeartAttributes.heartSize, height: HeartAttributes.heartSize))
         view.addSubview(heart)
         let fountainX = HeartAttributes.heartSize / 2.0 + 20
         let fountainY = view.bounds.height - HeartAttributes.heartSize / 2.0 - 10
-        heart.center = CGPoint(x: fountainX, y: fountainY)
+        heart.center = self.view.center
         heart.animateInView(view)
     }
     
